5edaa101350ca751c1c4d0881824cdc0bfe2c344
aydinefendi <aydinefendi27@gmail.com>
2024-09-09 17:16:35 +0300

    Reduced the value_area to 700 from 800

--- particles-config.js
+++ particles-config.js
@@ -1,6 +1,6 @@
 particlesJS('particles-js', {
     particles: {
-      number: { value: 80, density: { enable: true, value_area: 800 } },
+      number: { value: 80, density: { enable: true, value_area: 700 } },
       color: { value: "#ffffff" },
       shape: { type: "circle", stroke: { width: 0, color: "#000000" }, polygon: { nb_sides: 5 }, image: { src: "img/github.svg", width: 100, height: 100 } },
       opacity: { value: 0.5, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
